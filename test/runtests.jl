f37d79e632fa3efaa8b6e52b9b261502b2e0a632
Gregory Wagner <wagner.greg@gmail.com>
2018-10-06 13:20:46 +0100

    Smooths many bugs in NIWQG; adds new tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,6 +17,11 @@ import # use 'import' rather than 'using' for submodules to keep namespace clean
 using FourierFlows: lambdipole, parsevalsum, xmoment, ymoment
 using GeophysicalFlows.VerticallyFourierBoussinesq: mode1u
 
+const rtol_lambdipole = 1e-2 # tolerance for lamb dipole tests
+const rtol_niwqg = 1e-13 # tolerance for niwqg forcing tests
+const rtol_twodturb = 1e-13 # tolerance for niwqg forcing tests
+
+"Get the CFL number, assuming a uniform grid with `dx=dy`."
 cfl(U, V, dt, dx) = maximum([maximum(abs.(U)), maximum(abs.(V))]*dt/dx)
 cfl(prob) = cfl(prob.vars.U, prob.vars.V, prob.ts.dt, prob.grid.dx)
 
@@ -24,7 +29,7 @@ cfl(prob) = cfl(prob.vars.U, prob.vars.V, prob.ts.dt, prob.grid.dx)
 e1_fourier(u, v, p, m, N) = @. abs2(u) + abs2(v) + m^2*abs2(p)/N^2
 e1_fourier(prob) = e1_fourier(prob.vars.u, prob.vars.v, prob.vars.p, prob.params.m, prob.params.N)
 
-"Returns the x,y centroid of a cosine mode 1 internal wave in the Boussinesq equations."
+"Returns the `x,y` centroid of a cosine mode 1 internal wave in the Boussinesq equations."
 wavecentroid_fourier(prob) = (xmoment(e1_fourier(prob), prob.grid), ymoment(e1_fourier(prob), prob.grid))
                              
 "Returns the energy in vertically cosine mode 1 in the Boussinesq equations."
@@ -38,17 +43,17 @@ wavecentroid_cosine(prob) = (xmoment(e1_cosine(prob), prob.grid), ymoment(e1_cos
 ke_niwqg(phi) = @. abs2(phi)
 ke_niwqg(prob::AbstractProblem) = ke_niwqg(prob.vars.phi)
 
-"Returns the x,y centroid of the wave field kinetic energy in NIWQG."
+"Returns the `x,y` centroid of the wave field kinetic energy in NIWQG."
 wavecentroid_niwqg(prob) = (xmoment(ke_niwqg(prob), prob.grid), ymoment(ke_niwqg(prob), prob.grid))
 
 # Run tests
 testtime = @elapsed begin
 
-  #=
 @testset "TwoDTurb" begin
   include("test_twodturb.jl")
 end
 
+#=
 @testset "BarotropicQG" begin
   include("test_barotropicqg.jl")
 end
@@ -64,6 +69,17 @@ end
 
 @testset "NIWQG" begin
   include("test_niwqg.jl")
+
+  @test test_set_q()
+  @test test_set_phi()
+  @test test_niwqg_lambdipole(nx=256, dt=1e-3)
+  @test test_niwqg_groupvelocity(nkw=16)
+  @test test_niwqg_forcing_q(dt=0.01, stepper="RK4", nsteps=100)
+  @test test_niwqg_forcing_phi(dt=0.01, stepper="RK4", nsteps=100, muw=0, nu=0.01, nnu=0)
+  @test test_niwqg_forcing_phi(dt=0.01, stepper="RK4", nsteps=100, nu=0, muw=0.02, nmuw=1)
+  @test test_niwqg_nonlinear1(dt=0.01, stepper="RK4", nsteps=100)
+  @test test_niwqg_nonlinear2(dt=0.01, stepper="RK4", nsteps=10)
+  @test test_niwqg_wavepv(dt=0.01, stepper="RK4", nsteps=100)
 end
 
 end
--- test/test_niwqg.jl
+++ test/test_niwqg.jl
@@ -1,35 +1,23 @@
-#=
-function test_niwqg_lambdipole(n, dt; L=2π, Ue=1, Re=L/20, kap=0, nkap=1, ti=L/Ue*0.01, nm=3, message=false)
-  nt = round(Int, ti/dt)
-  prob = NIWQG.Problem(nx=n, Lx=L, kap=kap, nkap=nkap, dt=dt, stepper="FilteredRK4")
-  x, y, q = prob.grid.X, prob.grid.Y, prob.vars.q # nicknames
-
-  q0 = lambdipole(Ue, Re, prob.grid)
-  NIWQG.set_q!(prob, q0)
-
-  xq = zeros(nm)   # centroid of abs(Z)
-  Ue_m = zeros(nm) # measured dipole speed
-
-  # Step forward
-  for i = 1:nm
-    stepforward!(prob, nt)
-    NIWQG.updatevars!(prob)
-    xq[i] = mean(abs.(q).*x) / mean(abs.(q))
-
-    if i > 1
-      Ue_m[i] = (xq[i]-xq[i-1]) / ((nt-1)*dt)
-    else
-      Ue_m[i] = 0
-    end
-  end
-
-  isapprox(Ue, mean(Ue_m[2:end]), atol=0.02)
+function test_set_q()
+  prob = NIWQG.Problem()
+  q = rand(prob.grid.nx, prob.grid.ny)
+  qh = rfft(q)
+  NIWQG.set_q!(prob, q)
+  isapprox(q, prob.vars.q) && isapprox(qh, prob.state.solr)
 end
-=#
 
-function test_niwqg_lambdipole(n, dt; L=2π, Ue=1, Re=L/20, ti=L/Ue*0.01, nm=3)
+function test_set_phi()
+  prob = NIWQG.Problem()
+  phi = rand(prob.grid.nx, prob.grid.ny) + im*rand(prob.grid.nx, prob.grid.ny)
+  phih = fft(phi)
+  NIWQG.set_phi!(prob, phi)
+  isapprox(phi, prob.vars.phi) && isapprox(phih, prob.state.solc)
+end
+
+"Test nonlinear PV advection terms in NIWQG by measuring the propoagation speed of the Lamb dipole."
+function test_niwqg_lambdipole(; nx=256, dt=1e-3, Lx=2π, Ue=1, Re=Lx/20, ti=Lx/Ue*0.01, nm=3)
   nt = round(Int, ti/dt)
-  prob = NIWQG.Problem(nx=n, Lx=L, dt=dt, stepper="FilteredRK4")
+  prob = NIWQG.Problem(nx=nx, Lx=Lx, dt=dt, stepper="FilteredRK4")
   q0 = lambdipole(Ue, Re, prob.grid)
   NIWQG.set_q!(prob, q0)
 
@@ -45,23 +33,19 @@ function test_niwqg_lambdipole(n, dt; L=2π, Ue=1, Re=L/20, ti=L/Ue*0.01, nm=3)
       Ue_m[i] = (xq[i]-xq[i-1]) / ((nt-1)*dt)
     end
   end
-
-  println("Ue: $Ue, Ue_m: $(mean(Ue_m[2:end]))")
-
-  isapprox(Ue, mean(Ue_m[2:end]), rtol=1e-2)
+  isapprox(Ue, mean(Ue_m[2:end]), rtol=rtol_lambdipole)
 end
 
-
-
-function test_niwqg_groupvelocity(nkw; n=128, L=2π, f=1, eta=1/64, uw=1e-2, rtol=1e-3, del=L/10)
-  kw = nkw*2π/L
+"Test the group velocity of waves in NIWQG. This is essentially a test of the phi-dispersion term."
+function test_niwqg_groupvelocity(; nkw=16, nx=128, Lx=2π, f=1, eta=1/64, uw=1e-2, rtol=1e-3, del=Lx/10)
+  kw = nkw*2π/Lx
    σ = eta*kw^2/2
-  tσ = 2π/σ
+  tσ = 2π/(f+σ)
   dt = tσ/100
   nt = round(Int, 3tσ/(2dt)) # 3/2 a wave period
   cga = eta*kw # analytical group velocity
 
-  prob = NIWQG.Problem(f=f, eta=eta, nx=n, Lx=L, dt=dt, stepper="FilteredRK4")
+  prob = NIWQG.Problem(nx=nx, Lx=Lx, dt=dt, f=f, eta=eta, stepper="FilteredRK4")
   envelope(x, y) = exp(-x^2/(2*del^2))
   NIWQG.set_planewave!(prob, uw, nkw; envelope=envelope)
 
@@ -76,59 +60,144 @@ function test_niwqg_groupvelocity(nkw; n=128, L=2π, f=1, eta=1/64, uw=1e-2, rto
   isapprox(cga, cgn, rtol=rtol)
 end
 
-function test_niwqg_nonlinear1(nk=2; nx=32, Lx=2π, dt=0.1, nsteps=100, eta=0.2)
-  k = 2π/Lx * nk
+"Test q forcing."
+function test_niwqg_forcing_q(; dt=0.01, stepper="RK4", nsteps=1, nx=32, Lx=2π, eta=1, kap=0.01)
+  k = 4π/Lx # wavenumber 2
   g = TwoDGrid(nx, Lx)
-  phi0 = fill(1, (g.nx, g.ny))
-  q0 = @. sin(k*g.X)
+  tf = nsteps*dt
 
-  forcing = @. im/2*sin(k*g.X)
-  forcingh = fft(forcing)
+  q0 = @. sin(k*g.X)
+  qforcing = @. kap*q0
+  qforcingh = rfft(qforcing)
 
   function calcF!(Fc, Fr, t, s, v, p, g)
-    Fc .= forcing
+    @. Fr = qforcingh
     nothing
   end
 
-  prob = NIWQG.Problem(nx=nx, Lx=Lx, dt=dt, eta=eta, calcF=calcF!)
+  prob = NIWQG.Problem(nx=nx, Lx=Lx, dt=dt, eta=eta, calcF=calcF!, kap=kap, nkap=0, stepper=stepper)
+  NIWQG.set_q!(prob, q0)
+
+  stepforward!(prob, nsteps)
+  NIWQG.updatevars!(prob)
+
+  isapprox(prob.vars.q, q0, rtol=rtol_niwqg)
+end
+
+"Test phi diffusion, hyperdiffusion, dispersion and forcing."
+function test_niwqg_forcing_phi(; dt=0.01, stepper="RK4", nsteps=1, nx=32, Lx=2π, eta=0.0, nu=0.01, nnu=0,
+                               muw=0.02, nmuw=1)
+  k = 4π/Lx # wavenumber 2
+  g = TwoDGrid(nx, Lx)
+  tf = nsteps*dt
+
+  phi0 = @. exp(im*k*g.X) # nonlinear terms are 0
+  phiforcing = @. nu*k^(2nnu)*phi0 + muw*k^(2nmuw)*phi0 + 0.5*im*eta*k^2*phi0
+  phiforcingh = fft(phiforcing)
+
+  function calcF!(Fc, Fr, t, s, v, p, g)
+    @. Fc = phiforcingh
+    nothing
+  end
+
+  prob = NIWQG.Problem(nx=nx, Lx=Lx, dt=dt, eta=eta, nu=nu, nnu=nnu, muw=muw, nmuw=nmuw, 
+                       calcF=calcF!, stepper=stepper)
+  NIWQG.set_phi!(prob, phi0)
+
+  stepforward!(prob, nsteps)
+  NIWQG.updatevars!(prob)
+
+  isapprox(prob.vars.phi, phi0, rtol=rtol_niwqg)
+end
+
+"Test the refraction term in the phi-equation."
+function test_niwqg_nonlinear1(; dt=0.01, stepper="RK4", nsteps=100, nx=32, Lx=2π, kap=0.01)
+                               
+  k = 4π/Lx # wavenumber 2
+  g = TwoDGrid(nx, Lx)
+  tf = nsteps*dt
+
+  phi0 = fill(1, (g.nx, g.ny))
+  phif = phi0
+
+  q0 = @. sin(k*g.X)
+  qf = q0
+
+  phiforcing = @. 0.5*im*phi0*q0 # holds as long as phi=constant or phi=phi(x) with nu=0 and eta=0
+  phiforcingh = fft(phiforcing)
+
+  qforcing = @. kap*q0
+  qforcingh = rfft(qforcing)
+
+  function calcF!(Fc, Fr, t, s, v, p, g)
+    Fc .= phiforcingh
+    Fr .= qforcingh
+    nothing
+  end
+
+  prob = NIWQG.Problem(nx=nx, Lx=Lx, dt=dt, kap=kap, nkap=0, nu=0.01, nnu=1, calcF=calcF!, stepper=stepper)
   NIWQG.set_q!(prob, q0)
   NIWQG.set_phi!(prob, phi0)
 
   stepforward!(prob, nsteps)
   NIWQG.updatevars!(prob)
 
-  rtol = 1e-13
-  isapprox(prob.vars.q, q0, rtol=rtol) && isapprox(prob.vars.phi, phi0, rtol=rtol)
+  isapprox(prob.vars.q, qf, rtol=rtol_niwqg) && isapprox(prob.vars.phi, phif, rtol=rtol_niwqg)
 end
 
-function test_niwqg_nonlinear2(nk=2, nl=3; nx=32, Lx=2π, dt=0.1, nsteps=100, eta=0.2)
+"Test phi advection, refraction, and dispersion terms."
+function test_niwqg_nonlinear2(; stepper="RK4", nk=2, nl=3, nx=32, Lx=2π, dt=0.01, nsteps=100, 
+                               nu=0.01, eta=0.2, kap=0.02)
   k = 2π/Lx * nk
   l = 2π/Lx * nl
   g = TwoDGrid(nx, Lx)
   x, y = g.X, g.Y
+
   phi0 = @. exp(im*l*y)
   q0 = @. sin(k*x)
+  V0 = @. -cos(k*x)/k
 
-  forcing = @. im*exp(im*l*y) * (cos(k*x)*(0.5-l/k) + l^2*eta/2)
-  forcingh = fft(forcing)
+  phiforcing = @. 0.5*im*phi0*q0 + im*l*phi0*V0 + 0.5*im*eta*l^2*phi0 + nu*phi0
+  phiforcingh = fft(phiforcing)
+
+  qforcing = @. kap*q0
+  qforcingh = rfft(qforcing)
 
   function calcF!(Fc, Fr, t, s, v, p, g)
-    Fc .= forcing
+    @. Fc = phiforcingh
+    @. Fr = qforcingh
     nothing
   end
 
-  prob = NIWQG.Problem(nx=nx, Lx=Lx, dt=dt, eta=eta, calcF=calcF!)
+  prob = NIWQG.Problem(nx=nx, Lx=Lx, dt=dt, eta=eta, kap=kap, nkap=0, nu=nu, nnu=0, calcF=calcF!, stepper=stepper)
   NIWQG.set_q!(prob, q0)
   NIWQG.set_phi!(prob, phi0)
 
   stepforward!(prob, nsteps)
   NIWQG.updatevars!(prob)
 
-  rtol = 1e-13
-  isapprox(prob.vars.q, q0, rtol=rtol) && isapprox(prob.vars.phi, phi0, rtol=rtol)
+  isapprox(prob.vars.q, q0, rtol=rtol_niwqg) && isapprox(prob.vars.phi, phi0, rtol=rtol_niwqg)
 end
 
-@test test_niwqg_lambdipole(256, 1e-3)
-@test test_niwqg_groupvelocity(16)
-@test test_niwqg_nonlinear1()
-@test test_niwqg_nonlinear2()
+"Test wave PV."
+function test_niwqg_wavepv(; stepper="RK4", nk=2, nx=32, Lx=2π, f=1, dt=0.1, nsteps=100, nu=0.01)
+  k = 2π/Lx * nk
+  g = TwoDGrid(nx, Lx)
+  x, y = g.X, g.Y
+
+  phi0 = @. cos(k*x)
+  phiforcing = @. nu*phi0 + 0.125*im*k^2/f*(cos(k*x) + cos(3k*x))
+  phiforcingh = fft(phiforcing)
+
+  function calcF!(Fc, Fr, t, s, v, p, g)
+    Fc .= phiforcingh
+    nothing
+  end
+
+  prob = NIWQG.Problem(nx=nx, Lx=Lx, dt=dt, f=f, nu=nu, nnu=0, calcF=calcF!, stepper=stepper)
+  NIWQG.set_phi!(prob, phi0)
+  stepforward!(prob, nsteps)
+  NIWQG.updatevars!(prob)
+
+  isapprox(prob.vars.phi, phi0, rtol=rtol_niwqg)
+end
