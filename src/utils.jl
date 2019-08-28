d1d0571071bc79cc00aaf49d31191b3a06633465
Navid Constantinou <navidcy@gmail.com>
2019-08-29 08:44:16 +1000

    test_utils now work on both CPU/GPU

--- src/utils.jl
+++ src/utils.jl
@@ -22,20 +22,23 @@ Generate a real and random two-dimensional vorticity field q(x, y) with
 a Fourier spectrum peaked around a central non-dimensional wavenumber kpeak and
 normalized so that its total energy is E0.
 """
-function peakedisotropicspectrum(g::TwoDGrid, kpeak::Real, E0::Real; mask=ones(size(g.Krsq)), allones=false)
+function peakedisotropicspectrum(g::AbstractGrid{T, A}, kpeak::Real, E0::Real; mask=ones(size(g.Krsq)), allones=false) where {T, A}
   if g.Lx !== g.Ly
-      error("the domain is not square")
+    error("the domain is not square")
   else
     k0 = kpeak*2π/g.Lx
     modk = sqrt.(g.Krsq)
-    psik = zeros(g.nk, g.nl)
+    psik = A(zeros(T, (g.nk, g.nl)))
     psik = @. (modk^2 * (1 + (modk/k0)^4))^(-0.5)
     psik[1, 1] = 0.0
-    psih = @. randn(Complex{typeof(g.Lx)}, size(g.Krsq))*psik
+    phases = randn(Complex{T}, size(g.Krsq))
+    phases_real, phases_imag = real.(phases), imag.(phases)
+    phases = A(phases_real) + im*A(phases_imag)
+    psih = @. phases*psik
     if allones; psih = psik; end
-    psih = psih.*mask
+    psih = psih.*A(mask)
     Ein = real(sum(g.Krsq.*abs2.(psih)/(g.nx*g.ny)^2))
     psih = psih*sqrt(E0/Ein)
-    q = -irfft(g.Krsq.*psih, g.nx)
+    q = A(-irfft(g.Krsq.*psih, g.nx))
   end
 end
