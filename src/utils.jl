6898f57b0c838a1829885653ceab2671277a92de
Navid Constantinou <navidcy@gmail.com>
2019-08-30 08:42:07 +1000

    modifies lambdipole so that it can run on GPU

--- src/utils.jl
+++ src/utils.jl
@@ -4,14 +4,15 @@
 Return the 2D vorticity field of the Lamb dipole with strength `U` and radius `R`, centered on
 `center=(xc, yc)` and on the grid `g`. The default value of `center` is the middle of the grid.
 """
-function lambdipole(U, R, g; center=(mean(g.x), mean(g.y)))
+function lambdipole(U, R, g::AbstractGrid{T, A}; center=(mean(g.x), mean(g.y))) where {T, A}
   firstzero = 3.8317059702075123156
   k = firstzero/R # dipole wavenumber for radius R in terms of first zero of besselj
   q0 = -2U*k/besselj(0, k*R) # dipole amplitude for strength U and radius R
 
   xc, yc = center
   r = @. sqrt( (g.x-xc)^2 + (g.y-yc)^2 )
-  q = @. q0 * besselj(1, k*r) * (g.y-yc)/r
+  besselj1 = A([besselj(1, k*r[i, j]) for i=1:g.nx, j=1:g.ny])
+  q = @. q0 * besselj1 * (g.y-yc)/r
   @. q[r >= R] = 0
   q
 end
