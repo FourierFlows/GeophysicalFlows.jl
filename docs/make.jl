cfe6ee05372cef8169a8f54710e4f1650f529139
Navid Constantinou <navidcy@gmail.com>
2020-01-07 18:09:17 +1100

    adds Literate.jl in Docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,11 +1,41 @@
-# Workaround for JuliaLang/julia/pull/28625
-if Base.HOME_PROJECT[] !== nothing
-  Base.HOME_PROJECT[] = abspath(Base.HOME_PROJECT[])
-end
+push!(LOAD_PATH, "..")
 
 using
   Documenter,
+  Literate,
   GeophysicalFlows
+
+#####
+##### Generate examples
+#####
+
+const EXAMPLES_DIR = joinpath(@__DIR__, "..", "examples")
+const OUTPUT_DIR   = joinpath(@__DIR__, "src/generated")
+
+examples = [
+    "twodturb_mcwilliams1984.jl"
+]
+
+for example in examples
+    example_filepath = joinpath(EXAMPLES_DIR, example)
+    Literate.markdown(example_filepath, OUTPUT_DIR, documenter=true)
+end
+
+
+#####
+##### Build and deploy docs
+#####
+
+# Set up a timer to print a dot '.' every 60 seconds. This is to avoid Travis CI
+# timing out when building demanding Literate.jl examples.
+Timer(t -> println("."), 0, interval=60)
+
+format = Documenter.HTML(
+    collapselevel = 1,
+       prettyurls = get(ENV, "CI", nothing) == "true",
+        canonical = "https://fourierflows.github.io/GeophysicalFlows.jl/dev/"
+)
+
 
 makedocs(
  modules = [GeophysicalFlows],
@@ -16,12 +46,15 @@ checkdocs = :all,
  authors = "Navid C. Constantinou and Gregory L. Wagner",
 sitename = "GeophysicalFlows.jl",
    pages = Any[
-            "Home" => "index.md",
+            "Home"    => "index.md",
             "Modules" => Any[
               "modules/twodturb.md",
               "modules/barotropicqg.md",
               "modules/barotropicqgql.md",
               "modules/multilayerqg.md"
+            ],
+            "Examples" => [
+            "Two-dimensional turbulence"  => "generated/twodturb_mcwilliams1984.md"
             ],
             "DocStrings" => Any[
             "man/types.md",
