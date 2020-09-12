e3efe3187ef14be18d4f088d5eed8ec3685dad0b
Navid Constantinou <navidcy@gmail.com>
2020-09-12 10:18:05 +1000

    minor clean up of the forcing spectrum construction

--- examples/barotropicqgql_betaforced.jl
+++ examples/barotropicqgql_betaforced.jl
@@ -56,14 +56,15 @@ forcing_bandwidth  = 1.5     # the width of the forcing spectrum
 
 gr  = TwoDGrid(nx, Lx)
 
-k = [ gr.kr[i] for i=1:gr.nkr, j=1:gr.nl ] # a 2D grid with the zonal wavenumber
+K = @. sqrt(gr.Krsq)                      # a 2D array with the total wavenumber
+k = [gr.kr[i] for i=1:gr.nkr, j=1:gr.nl]  # a 2D array with the zonal wavenumber
 
-forcing_spectrum = @. exp( -(sqrt(gr.Krsq)-forcing_wavenumber)^2 / (2forcing_bandwidth^2) )
-@. forcing_spectrum[ gr.Krsq < (2π/Lx*2)^2  ] = 0
-@. forcing_spectrum[ gr.Krsq > (2π/Lx*20)^2 ] = 0
-@. forcing_spectrum[ k .< 2π/Lx ] .= 0 # make sure forcing does not have power at k=0 component
-ε0 = parsevalsum(forcing_spectrum .* gr.invKrsq/2, gr) / (gr.Lx*gr.Ly)
-@. forcing_spectrum = ε/ε0 * forcing_spectrum  # normalization so that forcing injects energy ε per domain area per unit time
+forcing_spectrum = @. exp(-(K - forcing_wavenumber)^2 / (2 * forcing_bandwidth^2))
+forcing_spectrum[K .< ( 2 * 2π/L)] .= 0   # no power at low wavenumbers
+forcing_spectrum[K .> (20 * 2π/L)] .= 0   # no power at high wavenumbers
+forcing_spectrum[k .< 2π/Lx ] .= 0        # make sure forcing does not have power at k=0
+ε0 = parsevalsum(forcing_spectrum .* grid.invKrsq / 2, grid) / (grid.Lx * grid.Ly)
+@. forcing_spectrum *= ε/ε0               # normalize forcing to inject energy at rate ε
 
 seed!(1234) # reset of the random number generator for reproducibility
 nothing # hide
