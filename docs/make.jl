e2b821efe1c71157f924be4b3e9a48d4f436b372
Navid Constantinou <navidcy@gmail.com>
2020-11-25 15:04:44 +1100

    removes barotropic_acc.jl example

--- docs/make.jl
+++ docs/make.jl
@@ -24,7 +24,6 @@ examples = [
     "twodnavierstokes_stochasticforcing_budgets.jl",
     "barotropicqg_betadecay.jl",
     "barotropicqg_betaforced.jl",
-    "barotropicqg_acc.jl",
     "barotropicqgql_betaforced.jl",
     "multilayerqg_2layer.jl",
     "surfaceqg_decaying.jl",
@@ -73,7 +72,6 @@ sitename = "GeophysicalFlows.jl",
               "BarotropicQG" => Any[
                 "generated/barotropicqg_betadecay.md",
                 "generated/barotropicqg_betaforced.md",
-                "generated/barotropicqg_acc.md",
                 ],
               "BarotropicQGQL" => Any[
                 "generated/barotropicqgql_betaforced.md",
