5b8cdaa977ee1741ba11f491c294ce5d6c51c2b7
Navid Constantinou <navidcy@gmail.com>
2021-04-06 12:15:04 +1000

    fix k²

--- src/multilayerqg.jl
+++ src/multilayerqg.jl
@@ -450,7 +450,7 @@ function calcS!(S, Fp, Fm, nlayers, grid)
   F = Matrix(Tridiagonal(Fm, -([Fp; 0] + [0; Fm]), Fp))
   
   for n=1:grid.nl, m=1:grid.nkr
-    CUDA.@allowscalar k² = grid.Krsq[m, n]
+    k² = CUDA.@allowscalar grid.Krsq[m, n]
     Skl = SMatrix{nlayers, nlayers}(- k² * I + F)
     S[m, n] = Skl
   end
