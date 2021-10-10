4736750f780e5af5d587b9b9ae772f11fa5e220d
Navid C. Constantinou <navidcy@users.noreply.github.com>
2021-10-11 10:01:13 +1100

    adds kwarg `devbranch="main"`
    required after renaming developer branch from `master`->`main`

--- docs/make.jl
+++ docs/make.jl
@@ -106,6 +106,7 @@ sitename = "GeophysicalFlows.jl",
 withenv("GITHUB_REPOSITORY" => "FourierFlows/GeophysicalFlowsDocumentation") do
   deploydocs(       repo = "github.com/FourierFlows/GeophysicalFlowsDocumentation.git",
                 versions = ["stable" => "v^", "v#.#", "dev" => "dev"],
-            push_preview = false
+            push_preview = false,
+               devbranch = "main"
             )
 end
