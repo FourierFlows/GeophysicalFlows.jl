efa0851e575c3659aaaa8378569b897f1f436268
Brodie Pearson <brodie.pearson@oregonstate.edu>
2022-01-03 14:14:54 -0800

    Write stochastic example w/tracers

--- examples/twodnavierstokes_stochasticforcing_tracer.jl
+++ examples/twodnavierstokes_stochasticforcing_tracer.jl
@@ -37,6 +37,7 @@ nothing # hide
     dt = 0.005               # timestep
 nsteps = 4000                # total number of steps
  nsubs = 20                  # number of steps between each plot
+ ntracers = 3   # number of tracers
 nothing # hide
 
 
@@ -76,7 +77,7 @@ nothing # hide
 random_uniform = dev==CPU() ? rand : CUDA.rand
 
 function calcF!(Fh, sol, t, clock, vars, params, grid) 
-  Fh .= sqrt.(forcing_spectrum) .* exp.(2π * im * random_uniform(eltype(grid), size(sol))) ./ sqrt(clock.dt)
+  Fh .= sqrt.(forcing_spectrum) .* exp.(2π * im * random_uniform(eltype(grid), size(sol[:,:,1]))) ./ sqrt(clock.dt)
   
   return nothing
 end
@@ -86,7 +87,7 @@ nothing # hide
 # ## Problem setup
 # We initialize a `Problem` by providing a set of keyword arguments. The
 # `stepper` keyword defines the time-stepper to be used.
-prob = TwoDNavierStokes.Problem(dev; nx=n, Lx=L, ν=ν, nν=nν, μ=μ, nμ=nμ, dt=dt, stepper="ETDRK4",
+prob = TwoDNavierStokesTracer.Problem(ntracers, dev; nx=n, Lx=L, ν=ν, nν=nν, μ=μ, nμ=nμ, dt=dt, stepper="ETDRK4",
                                 calcF=calcF!, stochastic=true)
 nothing # hide
 
@@ -122,14 +123,37 @@ heatmap(x, y, Array(irfft(vars.Fh, grid.nx)'),
 # ## Setting initial conditions
 
 # Our initial condition is a fluid at rest.
-TwoDNavierStokes.set_ζ!(prob, ArrayType(dev)(zeros(grid.nx, grid.ny)))
+
+ζ₀ = ArrayType(dev)(zeros(grid.nx, grid.ny))
+
+# Our initial condition for tracers (``c``).
+# For the first tracer we use a gaussian centered at ``(x, y) = (L_x/5, 0)``.
+gaussian(x, y, σ) = exp(-(x^2 + y^2) / (2σ^2))
+
+amplitude, spread = 0.5, 0.15
+for j in 1:ntracers
+  if j>1
+      global c₀ = cat(c₀, 0.0.*ζ₀, dims=3)
+  else
+      global c₀ = 0.0.*ζ₀
+  end
+end
+c₀[:,:,1] = [amplitude * gaussian(x[i] - 0.1 * grid.Lx, y[j], spread) for i=1:grid.nx, j=1:grid.ny]
+# For the second tracer we use a wider gaussian centered at a different location
+c₀[:,:,2] = [amplitude * gaussian(x[i] - 0.2 * grid.Lx, y[j] + 0.1 * grid.Ly, 4*spread) for i=1:grid.nx, j=1:grid.ny]
+# For the third tracer we use a straight band in the x-direction that is an eighth as wide as the domain
+width = 1/8
+[c₀[i,j,3] = amplitude for i=1:grid.nx, j=Int( (1/2) * grid.ny ):Int( (1/2 + width) * grid.ny )]
+
+TwoDNavierStokesTracer.set_ζ_and_tracers!(prob, ζ₀, c₀)
+nothing # hide
 
 
 # ## Diagnostics
 
 # Create Diagnostics; the diagnostics are aimed to probe the energy budget.
-E  = Diagnostic(TwoDNavierStokes.energy,                prob, nsteps=nsteps) # energy
-Z  = Diagnostic(TwoDNavierStokes.enstrophy,             prob, nsteps=nsteps) # enstrophy
+E  = Diagnostic(TwoDNavierStokesTracer.energy,                prob, nsteps=nsteps) # energy
+Z  = Diagnostic(TwoDNavierStokesTracer.enstrophy,             prob, nsteps=nsteps) # enstrophy
 diags = [E, Z] # a list of Diagnostics passed to `stepforward!` will  be updated every timestep.
 nothing # hide
 
@@ -140,7 +164,7 @@ nothing # hide
 # energy and enstrophy diagnostics. To plot energy and enstrophy on the same
 # axes we scale enstrophy with ``k_f^2``.
 
-p1 = heatmap(x, y, Array(vars.ζ'),
+p1 = heatmap(x, y, Array(vars.ζ[:,:,1]'),
          aspectratio = 1,
                    c = :balance,
                 clim = (-40, 40),
@@ -162,8 +186,48 @@ p2 = plot(2, # this means "a plot with two series"
                xlims = (0, 1.1 * μ * nsteps * dt),
                ylims = (0, 0.55))
 
-l = @layout Plots.grid(1, 2)
-p = plot(p1, p2, layout = l, size = (900, 420))
+ptimeϕ₁ = heatmap(x, y, Array(vars.ζ[:,:,2]'),
+            aspectratio = 1,
+                      c = :balance,
+                   clim = (-0.5, 0.5),
+                  xlims = (-L/2, L/2),
+                  ylims = (-L/2, L/2),
+                 xticks = -3:3,
+                 yticks = -3:3,
+                 xlabel = "x",
+                 ylabel = "y",
+                  title = "ϕ₁(x, y, t=" * @sprintf("%.2f", clock.t) * ")",
+             framestyle = :box)
+      
+  ptimeϕ₂ = heatmap(x, y, Array(vars.ζ[:,:,3]'),
+             aspectratio = 1,
+                       c = :balance,
+                    clim = (-0.5, 0.5),
+                   xlims = (-L/2, L/2),
+                   ylims = (-L/2, L/2),
+                  xticks = -3:3,
+                  yticks = -3:3,
+                  xlabel = "x",
+                  ylabel = "y",
+                   title = "ϕ₂(x, y, t=" * @sprintf("%.2f", clock.t) * ")",
+              framestyle = :box)
+      
+  ptimeϕ₃ = heatmap(x, y, Array(vars.ζ[:,:,4]'),
+              aspectratio = 1,
+                        c = :balance,
+                     clim = (-0.5, 0.5),
+                    xlims = (-L/2, L/2),
+                    ylims = (-L/2, L/2),
+                   xticks = -3:3,
+                   yticks = -3:3,
+                   xlabel = "x",
+                   ylabel = "y",
+                    title = "ϕ₃(x, y, t=" * @sprintf("%.2f", clock.t) * ")",
+               framestyle = :box)
+
+p = plot(p1, p2, ptimeϕ₁, ptimeϕ₂, ptimeϕ₃, layout=5, size = (1500, 800))
+#l = @layout Plots.grid(1, 2)
+#p = plot(p1, p2, layout = l, size = (900, 420))
 
 
 # ## Time-stepping the `Problem` forward
@@ -181,13 +245,16 @@ anim = @animate for j = 0:round(Int, nsteps / nsubs)
     println(log)
   end  
 
-  p[1][1][:z] = Array(vars.ζ)
+  p[1][1][:z] = Array(vars.ζ[:,:,1])
   p[1][:title] = "vorticity, μt = " * @sprintf("%.2f", μ * clock.t)
   push!(p[2][1], μ * E.t[E.i], E.data[E.i])
   push!(p[2][2], μ * Z.t[Z.i], Z.data[Z.i] / forcing_wavenumber^2)
+  p[3][1][:z] = Array(vars.ζ[:,:,2])
+  p[4][1][:z] = Array(vars.ζ[:,:,3])
+  p[5][1][:z] = Array(vars.ζ[:,:,4])
 
   stepforward!(prob, diags, nsubs)
-  TwoDNavierStokes.updatevars!(prob)  
+  TwoDNavierStokesTracer.updatevars!(prob)  
 end
 
-mp4(anim, "twodturb_forced.mp4", fps=18)
+mp4(anim, "twodturb_forced_tracer.mp4", fps=18)
